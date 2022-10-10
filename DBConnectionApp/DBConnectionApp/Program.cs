114c530ee5e7047bb35f44113f8ce8de3d80e592
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-10-10 23:10:56 +0900

    dbConnection / 프로시저 생성 필요

--- DBConnectionApp/DBConnectionApp/Program.cs
+++ DBConnectionApp/DBConnectionApp/Program.cs
@@ -5,14 +5,15 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Transactions;
 
 namespace DBConnectionApp
 {
     internal static class Program
     {
-        /// <summary>
-        /// 해당 애플리케이션의 주 진입점입니다.
-        /// </summary>
+        private static TransactionScopeAsyncFlowOption tranOpt;
+
         [STAThread]
         static void Main()
         {
@@ -20,43 +21,17 @@ namespace DBConnectionApp
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            string connectionString = "...";
-
-            IDbConnection connection = new SqlConnection(connectionString);
-
-            connection.Open();
-
-            IDbCommand command = new SqlCommand();
-
-            command.Connection = connection;
-
-            IDbTransaction transaction;
-
-            transaction = connection.BeginTransaction(); //Enlisting database
-
-            command.Transaction = transaction;
-
-            try
+            using (var scope = new TransactionScope(TransactionScopeOption.Required, tranOpt))
             {
 
-                /* 어쩌고 저쩌고 */
+                TransactionOptions tranOpt = new TransactionOptions();
+                tranOpt.IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted;  // IsolationLevel 셋팅
+                                                                                              //tranOpt.IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted;  // IsolationLevel 셋팅
 
-                transaction.Commit();
-
-            }
-
-            catch
-            {
-
-                transaction.Rollback(); //Abort transaction
-
-            }
-
-            finally
-            {
-
-                connection.Close();
-
+                using (TransactionScope scope1 = new TransactionScope(TransactionScopeOption.Required, tranOpt))
+                {
+                }
+                scope.Complete();
             }
         }
     }
